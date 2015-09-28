6cb1e2439bfe21c5015cd2ce07d49aa411de26db
Roberto Loja <robertoloja@gmail.com>
2015-09-28 15:43:30 -0400

    Included child processes to handle multiple user connections.

--- src/client.c
+++ src/client.c
@@ -5,6 +5,9 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+#define PORT_NUMBER 40000
+#define SERVER_NAME "localhost"
+
 void error(char *msg)
 {
     perror(msg);
@@ -13,35 +16,25 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    int sockfd, n;
 
     struct sockaddr_in serv_addr;
     struct hostent *server;
     char buffer[256];
 
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) 
         error((const char *) "ERROR opening socket");
 
-    server = gethostbyname(argv[1]);
-
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+    server = gethostbyname(SERVER_NAME);
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(PORT_NUMBER);
 
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error((const char *)"ERROR connecting");
--- src/server.c
+++ src/server.c
@@ -1,4 +1,3 @@
-// A simple server in the internet domain using TCP
 #include <stdio.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
@@ -9,6 +8,8 @@
 
 #define PORT_NUMBER 40000
 
+void chat(int sock);
+
 void error(char *msg)
 {
     perror(msg);
@@ -18,9 +19,9 @@ void error(char *msg)
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, clilen;
-     char buffer[1024];
+     char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
+     int n, pid;
 
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -35,12 +36,33 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
               error((const char *)"ERROR on binding");
 
+	 // This block listens and creates new sockets (per connection?)
+	 // N.B. that accept() halts execution until it makes a connection.
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-     if (newsockfd < 0) 
-          error((const char *) "ERROR on accept");
+	 while(1)
+	 {
+		 newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+		 if (newsockfd < 0) 
+			  error((const char *) "ERROR on accept");
+
+		 pid = fork();
+
+		 if (pid < 0){
+			 error((const char *) "ERROR on fork");
+		 }
+
+		 if (pid == 0)
+		 {
+			 close(sockfd);
+			 chat(newsockfd);
+			 exit(0);
+		 } else close(newsockfd);
+	 }
+	 /* The following couple of lines allow reading from the new socket.
+	    There is a cooresponding write() call in the client.
 
      bzero(buffer,1024);
      n = read(newsockfd,buffer,1023);
@@ -52,6 +74,26 @@ int main(int argc, char *argv[])
      n = write(newsockfd,"I got your message",18); // 18 is msg size
 
      if (n < 0) error((const char *) "ERROR writing to socket");
+	 */
 
      return 0; 
+}
+
+// This is the meat of the server. 
+// It should keep track of user names and route messages correctly.
+void chat(int sock)
+{
+	int n;
+	char buffer[256];
+
+	bzero(buffer, 256);
+	n = read(sock, buffer, 255);
+	printf("Read from socket\n");
+
+	if (n < 0)
+		error((const char *) "ERROR reading from socket");
+
+	n = write(sock, "i got the message", 17);
+	if (n < 0)
+		error((const char *) "ERROR writing to socket");
 }
