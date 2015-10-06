6f2cf316bde4861cd693c0ea240cb1da7d2ddc6e
Roberto Loja <robertoloja@gmail.com>
2015-10-06 15:00:11 -0400

    Added changeNick() and (currently empty) chat().

--- src/client.c
+++ src/client.c
@@ -2,26 +2,34 @@
 
 #define SERVER "localhost"
 
+int chat(User *userInfo);
+int changeNick(User *userInfo);
+
 int main(void)
 {
-	int sockfd, numbytes;
-	char msgBuffer[MAX_MSG_SIZE];
+	int sockfd;
+	//char msgBuffer[MAX_MSG_SIZE];
 	struct addrinfo hints, *servinfo, *res;
+	User user, *usrPtr = &user;
 	int rv;
 	char s[INET6_ADDRSTRLEN];
 
+	// Clear and populate the addrinfo hints.
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
+	// In case addrinfo fails.
 	if((rv = getaddrinfo(SERVER, PORT, &hints, &servinfo)) != 0)
 	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
 
+	// Navigate the linked list that starts at servinfo, to find valid addrs.
 	for(res = servinfo; res != NULL; res = res->ai_next)
 	{
+		// Check if successfully created a socket.
 		if((sockfd = socket(res->ai_family, res->ai_socktype, 
 						res->ai_protocol)) == -1)
 		{
@@ -29,6 +37,7 @@ int main(void)
 			continue;
 		}
 
+		// Check if successfully connected to socket.
 		if(connect(sockfd, res->ai_addr, res->ai_addrlen) == -1)
 		{
 			close(sockfd);
@@ -39,17 +48,36 @@ int main(void)
 		break;
 	}
 
+	// In case no valid address is found.
 	if(res == NULL)
 	{
 		fprintf(stderr, "client: failed to connect\n");
 		return 2;
 	}
 
+	// NetworkTOPresentation: Convert from local bit order to network bit order.
 	inet_ntop(res->ai_family, get_in_addr((struct sockaddr *) res->ai_addr),
 			s, sizeof s);
 	printf("client: connecting to %s", s);
 
+	// Don't need servinfo anymore; res points to the valid address.
 	freeaddrinfo(servinfo);
+	changeNick(usrPtr);
 
-	// All done with preliminary stuff. Actual client code goes in here.
+}
+
+int chat(User *userInfo)
+{
+	return 0;
+}
+
+int changeNick(User *userInfo)
+{
+	char name[MAX_NICK_LENGTH];
+
+	printf("Enter username: ");
+	scanf(" %s", name);
+	strcpy(name, userInfo->name);
+
+	return 0;
 }
